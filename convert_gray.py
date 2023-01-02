620186c67117fb69a670f721050c53d377db9a8a
AdarshMungra <adarshmungra1@gmail.com>
2023-01-02 18:07:19 +0400

    lab1

---
+++ convert_gray.py
@@ -0,0 +1,8 @@
+import cv2
+image = cv2.imread(
+    '/home/adarsh/PycharmProjects/pythonProject/test1/fruits.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Original image', image)
+cv2.imshow('Gray image', gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
